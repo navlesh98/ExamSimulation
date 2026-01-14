eea41f82f107717f63e680de17b354a51c789263
navlesh98 <navseebooa98@gmail.com>
2026-01-14 09:50:39 +0400

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,15 @@
-console.log("Timer loaded");
+let timeLeft;
+
+function startTimer(seconds) {
+  timeLeft = seconds;
+  const timer = document.getElementById("timer");
+
+  setInterval(() => {
+    timeLeft--;
+    timer.innerText = `Time left: ${Math.floor(timeLeft/60)}:${timeLeft%60}`;
+
+    if (timeLeft <= 0) {
+      window.location.href = "result.html";
+    }
+  }, 1000);
+}
